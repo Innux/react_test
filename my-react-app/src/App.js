b5872a5e12201f4503711e112e918cee1b0f1ace
Innux <innux_xu@163.com>
2018-09-30 16:51:12 +0800

    930

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,51 +1,9 @@
 import React from 'react'
-import { Layout, Menu, Icon } from 'antd'
-import QueueAnim from 'rc-queue-anim'
-
-const { Content, Sider } = Layout
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Layout >
-            <Sider
-            breakpoint="lg"
-            collapsedWidth="0"
-            >
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1">
-                        <Icon type="user" />
-                        <span className="nav-text">nav 1</span>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Icon type="video-camera" />
-                        <span className="nav-text">nav 2</span>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                        <Icon type="upload" />
-                        <span className="nav-text">nav 3</span>
-                    </Menu.Item>
-                    <Menu.Item key="4">
-                        <Icon type="user" />
-                        <span className="nav-text">nav 4</span>
-                    </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout>
-                {/* <Content style={{ margin: '12px 0' }}>
-                    <div style={{ padding: 24, background: '#fff', minHeight: 600 }}>
-                        <QueueAnim>
-                            <div key="demo1">依次进场</div>
-                            <div key="demo2">依次进场</div>
-                            <div key="demo3">依次进场</div>
-                            <div key="demo4">依次进场</div>
-                        </QueueAnim>
-                    </div>
-                </Content> */}
-            </Layout>
-        </Layout>
       </div>
     )
   }
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
 import 'antd/dist/antd.css';
 
+class Title extends Component {
+  render () {
+    return (
+      <h1>Title</h1>
+    )
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Header extends Component {
+  render () {
+    return (
+      <div>
+        <Title />
+      </div>
+    )
+  }
+}
+
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <h2>main....</h2>
+      </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render () {
+    return (
+      <div>
+        <h2>footer...</h2>
+      </div>
+    )
+  }
+}
+
+const NoStatus = (props) => {
+  const sayHi = (event) => {
+    console.log('hello')
+  }
+  return <div onClick={sayHi}>click</div>
+}
+
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Header/>
+        <Main/>
+        <NoStatus/>
+        <Footer/>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<Index />, document.getElementById('root'));

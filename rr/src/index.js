1702f581287efbee8d631b6c1e6a760661fc7f84
Innux <innux_xu@163.com>
2018-10-15 21:45:59 +0800

    1015

--- rr/src/index.js
+++ rr/src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
+import PropTypes from 'prop-types'
+import Header from './Header'
+import Content from './Content'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function createStore (reducer) {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({})
+  return { getState, dispatch, subscribe }
+}
+
+const themeReducer = (state, action) => {
+  if (!state) {
+    return { themeColor: 'red' }
+  }
+
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor}
+    default:
+      return state
+  }
+}
+
+const store = createStore(themeReducer)
+
+class Index extends React.Component {
+  static childContextTypes = {
+    store: PropTypes.object
+  }
+
+  getChildContext () {
+    return { store }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
